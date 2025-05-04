1966e6c6719d83747407372c6a0ccc68e68a57ba
Vicenzo Boroto <vbpompermayer@gmail.com>
2025-05-04 12:11:13 -0300

    Aula 03 - Objetos parte 1

--- IdeaProjects/lojinha2/src/main/java/br/com/lojinha/LojinhaApp.java
+++ IdeaProjects/lojinha2/src/main/java/br/com/lojinha/LojinhaApp.java
@@ -1,12 +1,9 @@
 package br.com.lojinha;
 
+import br.com.lojinha.pojo.Produto;
+
 public class LojinhaApp {
     public static void main(String[] args) {
-        String joias = "100 gr de Ouro 50k";
-        int minhaIdade = 23;
-        double meuSalario = 29500.99;
-        boolean minhaResposta = false;
-
-        System.out.println(minhaResposta);
+        Produto meuProduto = new Produto();
     }
 }
