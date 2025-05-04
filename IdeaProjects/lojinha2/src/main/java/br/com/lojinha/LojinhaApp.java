ee60f3cfa6a57b4c3bacf0fd54c1595f530491a1
Vicenzo Boroto <vbpompermayer@gmail.com>
2025-05-04 11:46:56 -0300

    Aula 02 - Variaveis

--- IdeaProjects/lojinha2/src/main/java/br/com/lojinha/LojinhaApp.java
+++ IdeaProjects/lojinha2/src/main/java/br/com/lojinha/LojinhaApp.java
@@ -2,6 +2,11 @@ package br.com.lojinha;
 
 public class LojinhaApp {
     public static void main(String[] args) {
-        System.out.println("Lojinha");
+        String joias = "100 gr de Ouro 50k";
+        int minhaIdade = 23;
+        double meuSalario = 29500.99;
+        boolean minhaResposta = false;
+
+        System.out.println(minhaResposta);
     }
 }
