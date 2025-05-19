1421468131216fa336572a99927e579b2cc1f698
Vicenzo Boroto <vbpompermayer@gmail.com>
2025-05-19 19:57:47 -0300

    Aula listas Java

--- IdeaProjects/lojinha2/src/main/java/br/com/lojinha/LojinhaApp.java
+++ IdeaProjects/lojinha2/src/main/java/br/com/lojinha/LojinhaApp.java
@@ -2,8 +2,24 @@ package br.com.lojinha;
 
 import br.com.lojinha.pojo.Produto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LojinhaApp {
     public static void main(String[] args) {
         Produto meuProduto = new Produto();
+
+        meuProduto.setNome("Playstation 4");
+        meuProduto.setValor(10);
+        meuProduto.setMarca("Sony");
+        meuProduto.setTamanho("Sony");
+
+        List<String> itensInclusos = new ArrayList<>();
+        itensInclusos.add("2 Controles");
+        itensInclusos.add("3 Jogos");
+        meuProduto.setItensInclusos(itensInclusos);
+
+        System.out.println(meuProduto.getValor());
+        System.out.println(meuProduto.getItensInclusos().size());
     }
 }
--- IdeaProjects/lojinha2/src/main/java/br/com/lojinha/pojo/Produto.java
+++ IdeaProjects/lojinha2/src/main/java/br/com/lojinha/pojo/Produto.java
@@ -1,9 +1,55 @@
 package br.com.lojinha.pojo;
 
+import java.util.List;
+
 public class Produto {
-    public String nome;
-    public String marca;
-    public double valor;
-    public String tamanho;
-    public String itensInclusos;
+    private String nome;
+    private String marca;
+    private double valor;
+    private String tamanho;
+    private List<String> itensInclusos;
+
+    public double getValor() {
+        return this.valor;
+    }
+
+    public void setValor(double novoValor) {
+        if(novoValor > 0) {
+            this.valor = novoValor;
+        } else {
+            throw new IllegalArgumentException("Valores devem ser maiores que 0.");
+        }
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String novoNome) {
+        this.nome = novoNome;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String novaMarca) {
+        this.marca = novaMarca;
+    }
+
+    public String getTamanho() {
+        return this.tamanho;
+    }
+
+    public void setTamanho(String novoTamanho) {
+        this.tamanho = novoTamanho;
+    }
+
+    public List<String> getItensInclusos() {
+        return this.itensInclusos;
+    }
+
+    public void setItensInclusos(List<String> novosItens) {
+        this.itensInclusos = novosItens;
+    }
 }
